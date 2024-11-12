f3c13108acd04b5f8c38f6e15ab1f61eab4454fc
JuneNguyen <156218862+NgocDungNguyen@users.noreply.github.com>
2024-11-12 15:04:35 +0700

    Add files via upload

--- src/main/java/com/rentalsystem/util/AsciiTableGenerator.java
+++ src/main/java/com/rentalsystem/util/AsciiTableGenerator.java
@@ -1,13 +1,19 @@
 package com.rentalsystem.util;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class AsciiTableGenerator {
-    private static final String HORIZONTAL_SEP = "-";
-    private static final String VERTICAL_SEP = "|";
-    private static final String JOIN_SEP = "+";
+    private static final String HORIZONTAL_SEP = "─";
+    private static final String VERTICAL_SEP = "│";
+    private static final String JOIN_SEP = "┼";
+    private static final String TOP_LEFT_CORNER = "┌";
+    private static final String TOP_RIGHT_CORNER = "┐";
+    private static final String BOTTOM_LEFT_CORNER = "└";
+    private static final String BOTTOM_RIGHT_CORNER = "┘";
+    private static final String TOP_JOIN = "┬";
+    private static final String BOTTOM_JOIN = "┴";
+    private static final String LEFT_JOIN = "├";
+    private static final String RIGHT_JOIN = "┤";
 
     public static String generateTable(String[] headers, List<String[]> data) {
         StringBuilder sb = new StringBuilder();
@@ -24,9 +30,10 @@ public class AsciiTableGenerator {
         }
 
         // Generate top border
-        sb.append(JOIN_SEP);
-        for (int width : maxWidths) {
-            sb.append(repeat(HORIZONTAL_SEP, width + 2)).append(JOIN_SEP);
+        sb.append(TOP_LEFT_CORNER);
+        for (int i = 0; i < maxWidths.length; i++) {
+            sb.append(repeat(HORIZONTAL_SEP, maxWidths[i] + 2));
+            sb.append(i == maxWidths.length - 1 ? TOP_RIGHT_CORNER : TOP_JOIN);
         }
         sb.append("\n");
 
@@ -38,9 +45,10 @@ public class AsciiTableGenerator {
         sb.append("\n");
 
         // Generate middle border
-        sb.append(JOIN_SEP);
-        for (int width : maxWidths) {
-            sb.append(repeat(HORIZONTAL_SEP, width + 2)).append(JOIN_SEP);
+        sb.append(LEFT_JOIN);
+        for (int i = 0; i < maxWidths.length; i++) {
+            sb.append(repeat(HORIZONTAL_SEP, maxWidths[i] + 2));
+            sb.append(i == maxWidths.length - 1 ? RIGHT_JOIN : JOIN_SEP);
         }
         sb.append("\n");
 
@@ -54,11 +62,79 @@ public class AsciiTableGenerator {
         }
 
         // Generate bottom border
-        sb.append(JOIN_SEP);
-        for (int width : maxWidths) {
-            sb.append(repeat(HORIZONTAL_SEP, width + 2)).append(JOIN_SEP);
+        sb.append(BOTTOM_LEFT_CORNER);
+        for (int i = 0; i < maxWidths.length; i++) {
+            sb.append(repeat(HORIZONTAL_SEP, maxWidths[i] + 2));
+            sb.append(i == maxWidths.length - 1 ? BOTTOM_RIGHT_CORNER : BOTTOM_JOIN);
         }
         sb.append("\n");
+
+        return sb.toString();
+    }
+
+    public static String generateMenuTable(String title, String[] options) {
+        StringBuilder sb = new StringBuilder();
+        int maxWidth = title.length();
+        for (String option : options) {
+            maxWidth = Math.max(maxWidth, option.length());
+        }
+
+        // Title
+        sb.append(TOP_LEFT_CORNER).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(TOP_RIGHT_CORNER).append("\n");
+        sb.append(VERTICAL_SEP).append(" ").append(padCenter(title, maxWidth + 2)).append(" ").append(VERTICAL_SEP).append("\n");
+        sb.append(LEFT_JOIN).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(RIGHT_JOIN).append("\n");
+
+        // Options
+        for (int i = 0; i < options.length; i++) {
+            sb.append(VERTICAL_SEP).append(" ").append(padRight(options[i], maxWidth + 2)).append(" ").append(VERTICAL_SEP).append("\n");
+        }
+
+        // Bottom border
+        sb.append(BOTTOM_LEFT_CORNER).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(BOTTOM_RIGHT_CORNER).append("\n");
+
+        return sb.toString();
+    }
+
+    public static String generateInputPromptTable(String title, String[] prompts) {
+        StringBuilder sb = new StringBuilder();
+        int maxWidth = title.length();
+        for (String prompt : prompts) {
+            maxWidth = Math.max(maxWidth, prompt.length());
+        }
+
+        // Title
+        sb.append(TOP_LEFT_CORNER).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(TOP_RIGHT_CORNER).append("\n");
+        sb.append(VERTICAL_SEP).append(" ").append(padCenter(title, maxWidth + 2)).append(" ").append(VERTICAL_SEP).append("\n");
+        sb.append(LEFT_JOIN).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(RIGHT_JOIN).append("\n");
+
+        // Prompts
+        for (int i = 0; i < prompts.length; i++) {
+            sb.append(VERTICAL_SEP).append(" ").append(padRight(prompts[i], maxWidth + 2)).append(" ").append(VERTICAL_SEP).append("\n");
+            if (i < prompts.length - 1) {
+                sb.append(LEFT_JOIN).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(RIGHT_JOIN).append("\n");
+            }
+        }
+
+        // Bottom border
+        sb.append(BOTTOM_LEFT_CORNER).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(BOTTOM_RIGHT_CORNER).append("\n");
+
+        return sb.toString();
+    }
+
+    public static String generateConfirmationTable(String title, String message) {
+        StringBuilder sb = new StringBuilder();
+        int maxWidth = Math.max(title.length(), message.length());
+
+        // Title
+        sb.append(TOP_LEFT_CORNER).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(TOP_RIGHT_CORNER).append("\n");
+        sb.append(VERTICAL_SEP).append(" ").append(padCenter(title, maxWidth + 2)).append(" ").append(VERTICAL_SEP).append("\n");
+        sb.append(LEFT_JOIN).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(RIGHT_JOIN).append("\n");
+
+        // Message
+        sb.append(VERTICAL_SEP).append(" ").append(padRight(message, maxWidth + 2)).append(" ").append(VERTICAL_SEP).append("\n");
+
+        // Bottom border
+        sb.append(BOTTOM_LEFT_CORNER).append(repeat(HORIZONTAL_SEP, maxWidth + 4)).append(BOTTOM_RIGHT_CORNER).append("\n");
 
         return sb.toString();
     }
@@ -69,5 +145,9 @@ public class AsciiTableGenerator {
 
     private static String padRight(String s, int n) {
         return String.format("%-" + n + "s", s);
+    }
+
+    private static String padCenter(String s, int width) {
+        return String.format("%-" + width + "s", String.format("%" + (s.length() + (width - s.length()) / 2) + "s", s));
     }
 }
